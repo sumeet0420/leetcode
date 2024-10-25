9d8a5097b85f6448e2fb11940b1a8de604f34193
Sumeet Agrawal <sumeet.agrawal@hcl.com>
2024-10-25 11:50:29 +0530

    Leetcode problem solving

--- src/com/learning/linkedlistandtree/LeetCode226_InvertTree.java
+++ src/com/learning/linkedlistandtree/LeetCode226_InvertTree.java
@@ -4,14 +4,18 @@ import com.learning.linkedlistandtree.predefine.TreeNode;
 
 public class LeetCode226_InvertTree {
 
+    int level=0;
     public TreeNode invertTree(TreeNode root) {
         //post-order traversal
         if (root == null) return null;
         if (root.left != null) invertTree(root.left);
         if (root.right != null) invertTree(root.right);
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
+        level++;
+        if(level%2!=0) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
         return root;
     }
 }
---
+++ src/com/learning/strings/LeetCode1170_CompareStringsByFreqOfSmallestCharacter.java
@@ -0,0 +1,31 @@
+package com.learning.strings;
+
+public class LeetCode1170_CompareStringsByFreqOfSmallestCharacter {
+
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] res = new int[queries.length];
+        int i=0;
+        for(String query:queries){
+            int count = 0;
+            int freqCompare = smallestLexicoFreqquency(query);
+            for(String word:words){
+                if(freqCompare<smallestLexicoFreqquency(word)) count++;
+            }
+            res[i++] = count;
+        }
+        return res;
+    }
+
+    private int smallestLexicoFreqquency(String query){
+        int[] freq = new int[26];
+        for(char ch:query.toCharArray()){
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return freq[i];
+            }
+        }
+        return -1;
+    }
+}
